3756afdeac7ac13382cf43541caddcc35352c3b8
Chase Hawthorne <ch73168@gmail.com>
2018-09-02 15:40:55 -0400

    Added instruction page

--- Code/game.js
+++ Code/game.js
@@ -13,6 +13,7 @@ game.state.add('boot', bootState);
 game.state.add('load', loadState);
 game.state.add('menu', menuState);
 game.state.add('play', playState);
+game.state.add('instructions', instructionState)
 
 // Start boot state
 game.state.start('boot');
